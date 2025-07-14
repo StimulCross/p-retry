4a692fb5d7f4be771adfb461ba23bb7335e20201
Stimul Cross <stimulcross@gmail.com>
2025-07-14 10:19:51 +0500

    feat: capture stack trace for abort error

--- src/errors/abort.error.ts
+++ src/errors/abort.error.ts
@@ -15,7 +15,12 @@
 export class AbortError extends Error {
 	constructor(message?: string, options?: ErrorOptions) {
 		super(message, options);
+
 		this.name = new.target.name;
 		Object.setPrototypeOf(this, new.target.prototype);
+
+		if (Error.captureStackTrace) {
+			Error.captureStackTrace(this, new.target);
+		}
 	}
 }
